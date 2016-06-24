74b6ee56fa7468aad19aa442dd797711a56c465d
bjolivot <bjolivot@gmail.com>
2016-06-24 15:56:56 +0200

    work in progress

---
+++ generate_playbook.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+display_usage() { 
+    echo "Bad arguments" 
+    echo -e "\nUsage:\n$0 new_playbook_name\n" 
+    } 
+
+
+if [  $# != 1 ] 
+then 
+    display_usage
+    exit 1
+fi 
+
+ansible-playbook generate_playbook.yml -e "new_playbook_name=$1"
